67b2bb00b44d901ed8bd303419e1874b75fdc4cb
Jas Mann <jasforshort@hotmail.ca>
2022-10-27 19:02:47 -0400

    Work on props and state within Form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,14 @@ export default function Form(props) {
   const [interviewer, setInterviewer]= useState(props.interviewer || null);
   const [name, setName] = useState(props.name || "");
 
+  const reset = function(){
+    setName("");
+    setInterviewer(null);
+  }
+  const cancel = function(){
+    reset();
+  }
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -23,12 +31,12 @@ export default function Form(props) {
           */
          />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} interviewer={interviewer} onChange={setInterviewer} />
         </section>
         <section className="appointment__card-right">
           <section className="appointment__actions">
             <Button danger onClick={props.onCancel}>Cancel</Button>
-            <Button confirm onClick={props.onSave}>Save</Button>
+            <Button confirm onClick={cancel}>Save</Button>
           </section>
         </section>
     </main> 
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment() {
-
   return (
     <article className="appointment">
 
