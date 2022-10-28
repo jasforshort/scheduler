9cab0fba94200024f28266a058a19a90cc894625
Jas Mann <jasforshort@hotmail.ca>
2022-10-28 19:28:41 +0000

    Use our custom hook

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,14 +6,6 @@ export default function Form(props) {
   const [interviewer, setInterviewer]= useState(props.interviewer || null);
   const [name, setName] = useState(props.name || "");
 
-  const reset = function(){
-    setName("");
-    setInterviewer(null);
-  }
-  const cancel = function(){
-    reset();
-  }
-
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -35,8 +27,8 @@ export default function Form(props) {
         </section>
         <section className="appointment__card-right">
           <section className="appointment__actions">
-            <Button danger onClick={props.onCancel}>Cancel</Button>
-            <Button confirm onClick={cancel}>Save</Button>
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+            <Button confirm onClick={props.onSave}>Save</Button>
           </section>
         </section>
     </main> 
