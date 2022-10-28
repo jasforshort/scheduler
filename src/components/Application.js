4e1451b11265850bb6e49994de9517baa92e5842
Jas Mann <jasforshort@hotmail.ca>
2022-10-28 18:21:21 +0000

    Import axios and useEffect to fetch data from API

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from "components/DayList";
@@ -34,30 +35,28 @@ const appointments = [
         avatar: "https://i.imgur.com/LpaY82x.png",
       }
     }
-  }
+  },
+  {
+    id: 4,
+    time: "3pm",
+  },
+  {
+    id: 5,
+    time: "4pm",
+  },
 ];
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  useEffect(()=>{
+    const dayURL = "http://localhost:8001/api/days";
+    axios.get(dayURL).then(response =>{
+      console.log(response.data)
+      setDays([...response.data]);
+    })
+  },[]);
 
   return (
     <main className="layout">
